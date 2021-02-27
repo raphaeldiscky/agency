a5c44f84dc2908fe1221f58ee5db91d1355a64a7
Raphael Discky <zundria.putra@gmail.com>
2021-02-27 11:13:13 +0700

    Create banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { useEffect } from 'react'
+import Banner from './components/Banner'
 import Header from './components/Header'
 import './styles/App.scss'
 
 function App() {
+  // viewport unit on mobile
+  useEffect(() => {
+    let vh = window.innerHeight * 0.01
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
+  }, [])
+
   return (
     <div className='App'>
       <Header />
+      <Banner />
     </div>
   )
 }
