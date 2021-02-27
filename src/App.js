7c652a8810d61c42407e7d70670bb23938e185a5
Raphael Discky <zundria.putra@gmail.com>
2021-02-27 18:09:03 +0700

    Create intro overlay

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import Banner from './components/Banner'
 import Cases from './components/Cases'
 import Header from './components/Header'
+import IntroOverlay from './components/IntroOverlay'
 import './styles/App.scss'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
 
   return (
     <div className='App'>
+      <IntroOverlay />
       <Header />
       <Banner />
       <Cases />
