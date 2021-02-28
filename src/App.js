ba9da5c7f43495f5bf6e43bcc490f9b405c80b07
Raphael Discky <zundria.putra@gmail.com>
2021-02-28 14:14:13 +0700

    Update viewport responsiveness from mobile to desktop

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import gsap from 'gsap'
 import './styles/App.scss'
@@ -14,19 +14,49 @@ import Approach from './pages/approach'
 import Navigation from './components/Navigation'
 import Header from './components/Header'
 
+function debounce(fn, ms) {
+  let timer
+  return () => {
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      timer = null
+      fn.apply(this, arguments)
+    }, ms)
+  }
+}
+
 function App() {
+  // update viewport responsiveness from mobile to desktop
+  const [dimensions, setDimensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth
+  })
+
   useEffect(() => {
     // prevent flashing
     gsap.to('body', 0, { css: { visibility: 'visible' } })
 
     // set viewport unit on mobile
-    let vh = window.innerHeight * 0.01
+    let vh = dimensions.height * 0.01
     document.documentElement.style.setProperty('--vh', `${vh}px`)
+
+    const debouncedHandleResize = debounce(function handleResize() {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth
+      })
+    }, 1000)
+
+    window.addEventListener('resize', debouncedHandleResize)
+    return () => {
+      window.removeEventListener('resize', debouncedHandleResize)
+    }
   }, [])
 
   return (
     <Router>
       <Header />
+      {console.log(dimensions)}
       <div className='App'>
         <Route exact path='/' component={Home} />
         <Route exact path='/case-studies' component={CaseStudies} />
